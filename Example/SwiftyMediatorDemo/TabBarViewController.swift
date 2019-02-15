1d5d2ceddf0fa44871a9129c122600e41c4a7e2f
shayuan <shayuan@silvrr.com>
2019-02-15 15:38:03 +0800

    m: 添加注释

--- Example/SwiftyMediatorDemo/TabBarViewController.swift
+++ Example/SwiftyMediatorDemo/TabBarViewController.swift
@@ -14,10 +14,13 @@ class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // 如果需要做动态化，需要支持url跳转，那么需要做注册
         Mediator.register(ModuleAMediatorType.self)
+        // 如果需要做动态化，需要对已有的url做替换，那么需要使用这个方法
         Mediator.replace(url: "sy://detail", with: "sy://home?title=Replaced")
         
+        // 如果不需要从url获取viewController，那么可以使用枚举来获取，如下
+        // Mediator.viewController(of: ModuleAMediatorType.home(title: "Home"))
         guard let home = Mediator.viewController(of: "sy://home?title=Home"),
             let personal = Mediator.viewController(of: ModuleBMediatorType.personal(color: .blue)) else { return }
         
--- Example/SwiftyMediatorDemo/ViewController.swift
+++ Example/SwiftyMediatorDemo/ViewController.swift
@@ -21,7 +21,13 @@ class ViewController: UIViewController {
     }
     
     @IBAction func push(_ sender: Any) {
+        
+        // 使用url来push，注意：因为在`TabBarViewController`中已经将这个url做了替换，所以推的是Home
         Mediator.push("sy://detail")
+        
+        // 也可以这样来推
+        // Mediator.push(ModuleAMediatorType.home(title: "Home-Pushed"))
+        
     }
   
 
